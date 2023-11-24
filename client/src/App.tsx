535bbee1ecebb28e01b1028014e67defee5d5124
Yarden-Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-11-24 10:13:19 +0200

    backup commit

---
+++ client/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("localhost:5000/api/weather")
+      .then(res => res.json())
+      .then(data => setData(data));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        {data}
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
+from dotenv import load_dotenv
 import os
 import requests
+from flask import Flask
+
+app = Flask(__name__)
+load_dotenv()
 
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
+
 city = input("Enter a city name: ")
-req_url = f"{BASE_URL}?appid={os.environ['API_KEY']}&q={city}"
+req_url = f"{BASE_URL}?appid={os.getenv('API_KEY')}&q={city}"
 res = requests.get(req_url)
 
 
@@ -14,3 +20,13 @@ def get_data():
         return data
     else:
         print("Error fetching data.")
+
+
+@app.route("/weather")
+def weather():
+    data = get_data()
+    return data
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
